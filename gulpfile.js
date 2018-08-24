bc00e3d8811aae1b58b432aa3b02e9d1d92b32ca
Ben N <bentinata@gmail.com>
2018-08-24 18:06:04 +0700

    Move according base repo.

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,14 @@ const { public_dir } = require('./config');
 const dest = `${public_dir}/lib`;
 const gulp = require('gulp');
 
-gulp.task('copy_libs', () =>
-  gulp
+gulp.task('copy_theme', () => gulp
+  .src([
+    'themes/default/**/*',
+  ])
+  .pipe(gulp.dest(dest))
+);
+
+gulp.task('copy_libs', () => gulp
   .src([
     'node_modules/bootstrap/**/*',
     'node_modules/bootstrap-rtl/**/*',
@@ -20,11 +26,10 @@ gulp.task('copy_libs', () =>
 );
 
 
-gulp.task('copy_css', () =>
-  gulp
-  .src([ 'css/**/*' ])
+gulp.task('copy_css', () => gulp
+  .src(['css/**/*'])
   .pipe(gulp.dest(dest))
 );
 
 // Default
-gulp.task('default', ['copy_libs', 'copy_css']);
+gulp.task('default', ['copy_theme', 'copy_libs', 'copy_css']);
